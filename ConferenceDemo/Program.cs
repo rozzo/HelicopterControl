a213b36b5743a8bd6e8dcc28220771404db72a30
Matt Schmidt <matt.schmidt@omniresources.com>
2016-08-03 11:51:09 -0500

    modified commands for conference demo

--- ConferenceDemo/Program.cs
+++ ConferenceDemo/Program.cs
@@ -5,25 +5,22 @@ namespace ConferenceDemo
     {
         static void Main(string[] args)
         {
-            var helicopter = new Helicopter(150);
+            var helicopter = new Helicopter(120);
 
-            userCode(helicopter);
             //demo(helicopter);
+            userCode(helicopter);
         }
 
         private static void userCode(Helicopter helicopter)
         {
             helicopter.TakeOff();
 
-            helicopter.ChangeThrottle(600);
+            helicopter.ChangeThrottle(550);
 
             //put your commands here:
             //type 'helicopter.' and then see the list of commands you can do.
-            helicopter.MoveForward(2000);
+            helicopter.sleep(10000);
 
-            helicopter.TurnRight();
-
-            helicopter.MoveForward(2000);
             
             helicopter.Land();
         }
@@ -34,19 +31,21 @@ namespace ConferenceDemo
             helicopter.TakeOff();
 
             //Throttle command - LIFT
-            helicopter.ChangeThrottle(500);
+            helicopter.ChangeThrottle(550);
 
             //Move forward - watch out im moving forward
-            helicopter.MoveForward();
+            helicopter.MoveForward(1000);
 
             //Move backward - backing up!
             helicopter.MoveBackward(2000);
 
             //Turn Left - Think about looking out of the helicopter window
-            //helicopter.TurnLeft();
+            helicopter.TurnLeft(2000);
+
+            helicopter.ChangeThrottle(540);
 
             //Turn right - Think about looking out of the helicopter window
-            //helicopter.TurnRight();
+            helicopter.TurnRight(2000);
 
             //Landing command
             helicopter.Land();
